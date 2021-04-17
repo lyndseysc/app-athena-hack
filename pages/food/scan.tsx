2c3e595f1381295fd5433cdc875be86c17cf8c4c
lyndseysc <lyndsey.736@gmail.com>
2021-04-17 12:45:44 +0100

    merge changes

---
+++ pages/food/scan.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+// import { Input } from '@chakra-ui/react';
+
+const FoodScan = () => <div>food scan </div>;
+
+export default FoodScan;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
-import Head from 'next/head';
-import {getProducts} from "../api/getProducts"
+import { Button } from '@chakra-ui/react';
+import Link from 'next/link';
+import { getProducts } from '../api/getProducts';
 
 const App = () => {
-    const products = getProducts('chicken sausage')
-    console.log(products, "PRODUCST !!!!!!!!")
-return <div>hello</div>};
+  const products = getProducts('chicken sausage');
+  console.log(products, 'PRODUCST !!!!!!!!');
+
+  return (
+    <>
+      <div>hello</div>
+      <Link href='/food/scan'>
+        <Button>Scan my food</Button>
+      </Link>
+    </>
+  );
+};
 
 export default App;
