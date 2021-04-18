72e057a22bf64fe4a89d57c014e5642daf38193d
Martha Lambert <marthalambert@marthas-mbp.home>
2021-04-18 11:37:54 +0100

    feat: search type choice

--- pages/food/scan.tsx
+++ pages/food/scan.tsx
@@ -1,12 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { Input, Button, ChakraProvider } from '@chakra-ui/react';
+import { Input, Button, ChakraProvider, Flex, Text } from '@chakra-ui/react';
 import { getProductSuggestions } from 'api/getProductSuggestions';
 import { AnimatedBadge } from '../../styles/index.styles';
 
 const FoodScan = () => {
+  const [searchType, setSearchType] = useState<any>();
   const [searchTerm, setSearchTerm] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+
+  const searchOptions = () => (
+    <Flex>
+      <Text>Search for a food</Text>
+      <Button>Scan</Button>
+      <Button onClick={() => setSearchType('text')}>Text Search</Button>
+    </Flex>
+  );
+
+  const textSearch = () => (
+    <Flex>
+      <Input onChange={(event) => handleSearch(event)} />
+      {suggestions.map((suggestion) => (
+        <Link href={`/food/profile/${suggestion.id}`}>
+          <Button variant='solid' colorScheme='green' ml='1'>
+            {suggestion.name}
+          </Button>
+        </Link>
+      ))}
+    </Flex>
+  );
 
   useEffect(() => {
     if (searchTerm.length > 3) {
@@ -22,15 +44,7 @@ const FoodScan = () => {
 
   return (
     <ChakraProvider>
-      <Input onChange={(event) => handleSearch(event)} />
-
-      {suggestions.map((suggestion) => (
-        <Link href={`/food/profile/${suggestion.id}`}>
-          <Button variant='solid' colorScheme='green' ml='1'>
-            {suggestion.name}
-          </Button>
-        </Link>
-      ))}
+      {searchType === 'text' ? textSearch() : searchOptions()}
     </ChakraProvider>
   );
 };
